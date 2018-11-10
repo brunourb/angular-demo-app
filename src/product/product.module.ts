cfd74a4536bf08ba33e1b81c6453694b4cca82b4
Bruno Rodrigues <bruno@urbano.eti.br>
2018-11-10 13:29:37 -0200

    Initial commit

---
+++ src/product/product.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  declarations: [ProductComponent, ProductListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductModule { }
---
+++ src/product/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id : BigInteger,
+    codBarra : String,
+    name : String,
+    description : String,
+    value : number
+}
